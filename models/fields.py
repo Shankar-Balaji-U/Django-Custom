74e19473fb5ca503c9f3c71d1fa14a76dc9a288a
Shankar Balaji <72439750+funnyguy369@users.noreply.github.com>
2022-11-25 18:16:41 +0530

    Updated UIDAI to Aadhar added GST and PAN Fields.
    Modified UIDAI and Created newely GST and PAN fields

--- models/fields.py
+++ models/fields.py
@@ -1,102 +1,184 @@
+# Django imports
+from django.db import connection
 from django.db.models import Field
 from django.forms.fields import CharField
 from django.forms.widgets import NumberInput
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-from django.core import checks, exceptions, validators
-from django.db import connection, connections, router
+from django.core.validators import (
+            MaxLengthValidator, MinLengthValidator, 
+            ProhibitNullCharactersValidator )
+from django.utils.functional import cached_property
 
-	#     'AutoField': 'integer'
-	#     'BigAutoField': 'integer'
-	#     'BinaryField': 'BLOB'
-	#     'BooleanField': 'bool'
-	#     'CharField': 'varchar(%(max_length)s)'
-	#     'DateField': 'date'
-	#     'DateTimeField': 'datetime'
-	#     'DecimalField': 'decimal'
-	#     'DurationField': 'bigint'
-	#     'FileField': 'varchar(%(max_length)s)'
-	#     'FilePathField': 'varchar(%(max_length)s)'
-	#     'FloatField': 'real'
-	#     'IntegerField': 'integer'
-	#     'BigIntegerField': 'bigint'
-	#     'IPAddressField': 'char(15)'
-	#     'GenericIPAddressField': 'char(39)'
-	#     'JSONField': 'text'
-	#     'NullBooleanField': 'bool'
-	#     'OneToOneField': 'integer'
-	#     'PositiveBigIntegerField': 'bigint unsigned'
-	#     'PositiveIntegerField': 'integer unsigned'
-	#     'PositiveSmallIntegerField': 'smallint unsigned'
-	#     'SlugField': 'varchar(%(max_length)s)'
-	#     'SmallAutoField': 'integer'
-	#     'SmallIntegerField': 'smallint'
-	#     'TextField': 'text'
-	#     'TimeField': 'time'
-	#     'UUIDField': 'char(32)'
+from .models.validators import VerhoeffValidator, GSTINValidator, PANValidator
 
 
+class AadhaarField(Field):
+    empty_strings_allowed = False
+    default_error_messages = {
+        'invalid': _('“%(value)s” is not a valid Aadhaar Number.'),
+    }
+    description = _('Unique Identification Authority of India')
+
+    def __init__(self, verbose_name=None, **kwargs):
+        kwargs['max_length'] = 12
+        super().__init__(verbose_name, **kwargs)
+
+    @cached_property
+    def validators(self):
+        # Just for readability
+        validators_ = super().validators
+        min_len, max_len = self.max_length, self.max_length
+        validators_.append(VerhoeffValidator(message=self.error_messages['invalid']))
+        return validators_
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
+
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        if value is None:
+            return None
+        try:
+            return int(value)
+        except (TypeError, ValueError) as e:
+            raise e.__class__(
+                "Field '%s' expected a number but got %r." % (self.name, value),
+            ) from e
+
+    def to_python(self, value):
+        if value is None:
+            return value
+        try:
+            return str(value)
+        except (TypeError, ValueError):
+            raise ValidationError(
+                self.error_messages['invalid'],
+                code='invalid',
+                params={'value': value},
+            )
+
+    def get_internal_type(self):
+        return "IntegerField"
+
+    def formfield(self, **kwargs):
+        defaults = {'max_length': self.max_length}
+        if self.null and not connection.features.interprets_empty_strings_as_nulls:
+            defaults['empty_value'] = None
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
+
+
+class GSTField(Field):
+    default_error_messages = {
+        'invalid': _('“%(value)s” is not a valid GST Number.'),
+    }
+    description = _('Goods and Service Tax.')
+    empty_strings_allowed = False
+
+    def __init__(self, verbose_name=None, **kwargs):
+        kwargs['max_length'] = 15
+        super().__init__(verbose_name, **kwargs)
+
+    @cached_property
+    def validators(self):
+        # Just for readability
+        validators_ = super().validators
+        min_length, max_length = self.max_length, self.max_length
+        validators_.append(GSTINValidator(message=self.error_messages['invalid']))
+        return validators_
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
+
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        if value is None:
+            return None
+        try:
+            return str(value)
+        except (TypeError, ValueError) as e:
+            raise e.__class__(
+                "Field '%s' expected a string but got %r." % (self.name, value),
+            ) from e
+
+    def to_python(self, value):
+        if value is None:
+            return value
+        try:
+            return str(value)
+        except (TypeError, ValueError):
+            raise ValidationError(
+                self.error_messages['invalid'],
+                code='invalid',
+                params={'value': value},
+            )
+
+    def get_internal_type(self):
+        return "CharField"
+
+    def formfield(self, **kwargs):
+        defaults = {'max_length': self.max_length}
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
 
 
 
+class PANField(Field):
+    default_error_messages = {
+        'invalid': _('“%(value)s” is not a valid PAN Number.'),
+    }
+    description = _('Permanant Account Number.')
+    empty_strings_allowed = False
 
-class UIDAIField(Field):
-	default_error_messages = {
-		'invalid': _('“%(value)s” is not a valid Aadhaar Number.'),
-	}
-	description = _('Unique Identification Authority of India')
-	empty_strings_allowed = False
+    def __init__(self, verbose_name=None, **kwargs):
+        kwargs['max_length'] = 15
+        super().__init__(verbose_name, **kwargs)
 
-	def __init__(self, verbose_name=None, **kwargs):
-		kwargs['max_length'] = 12
-		super().__init__(verbose_name, **kwargs)
+    @cached_property
+    def validators(self):
+        # Just for readability
+        validators_ = super().validators
+        min_length, max_length = self.max_length, self.max_length
+        validators_.append(PANValidator(message=self.error_messages['invalid']))
+        return validators_
 
-	def deconstruct(self):
-		name, path, args, kwargs = super().deconstruct()
-		del kwargs['max_length']
-		return name, path, args, kwargs
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
 
-	def to_python(self, value):
-		"""Return a integer."""
-		if value not in self.empty_values:
-			if isinstance(value, int): value = str(value)
-			try:
-				if self._verhoeffvalidator(value): return int(value)
-			except ValueError:
-				raise exceptions.ValidationError(
-					self.error_messages['invalid'],
-					code='invalid',
-					params={'value': value},
-				)
-		return value
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        if value is None:
+            return None
+        try:
+            return str(value)
+        except (TypeError, ValueError) as e:
+            raise e.__class__(
+                "Field '%s' expected a string but got %r." % (self.name, value),
+            ) from e
 
-	def _verhoeffvalidator(self, value):
-		""" m => multiplication  , p => permutation """
-		m = (	(0,1,2,3,4,5,6,7,8,9), (1,2,3,4,0,6,7,8,9,5), 
-				(2,3,4,0,1,7,8,9,5,6), (3,4,0,1,2,8,9,5,6,7), 
-				(4,0,1,2,3,9,5,6,7,8), (5,9,8,7,6,0,4,3,2,1),
-				(6,5,9,8,7,1,0,4,3,2), (7,6,5,9,8,2,1,0,4,3), 
-				(8,7,6,5,9,3,2,1,0,4), (9,8,7,6,5,4,3,2,1,0) )
+    def to_python(self, value):
+        if value is None:
+            return value
+        try:
+            return str(value)
+        except (TypeError, ValueError):
+            raise ValidationError(
+                self.error_messages['invalid'],
+                code='invalid',
+                params={'value': value},
+            )
 
-		p = (	(0,1,2,3,4,5,6,7,8,9), (1,5,7,6,2,8,3,0,9,4), 
-				(5,8,0,3,7,9,6,1,4,2), (8,9,1,6,0,4,3,5,2,7), 
-				(9,4,5,3,1,2,6,8,7,0), (4,2,8,6,5,7,3,9,0,1),
-				(2,7,9,3,8,0,6,4,1,5), (7,0,4,6,9,1,3,2,5,8) )
-		i = len(value)
-		j = x = 0
-		while i > 0:
-			i -= 1
-			x = m[x][p[(j % 8)][int(value[i])]]
-			j += 1
-		if x == 0:
-			return True
-		raise ValueError
+    def get_internal_type(self):
+        return "CharField"
 
-	def get_internal_type(self):
-		return "IntegerField"
-
-	def formfield(self, **kwargs):
-		defaults = {'max_length': self.max_length}
-		if self.null and not connection.features.interprets_empty_strings_as_nulls:
-			defaults['empty_value'] = None
-		defaults.update(kwargs)
-		return super().formfield(**defaults)
+    def formfield(self, **kwargs):
+        defaults = {'max_length': self.max_length}
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
